2978d8ed67d5a89aa1f42a7a7c2fe1c3c925af13
github-mariyaweb <96mariyask@gmail.com>
2023-01-27 09:18:09 +0300

    fix: change algoritm from stack into if

--- src/index.js
+++ src/index.js
@@ -15,28 +15,27 @@ const BRACKETS_PAIR = {
 }
 let stack = [];
 module.exports = function check(str, bracketsConfig) {
-   //перебираем каждый элемент строки
+   let count = 0;
+
    for (let i = 0; i < str.length; i++) {
-      let currentBreacket = str[i];
 
-      //делаем проверку, открывающая ли это стрелка. Если да, то добавляем её в стэк. Если нет, то скобка не добавляется в стэк. Т.к. нет открывающей скобки - это уже ошибка, поэтому возвращаем false
-      if (OPEN_BRACKETS.includes(currentBreacket)) {
-         stack.push(currentBreacket);
-      } else {
-         if (stack.length === 0) {
-            return false;
-         }
+      if (str[i] == '(' || str[i] == '{' || str[i] == '|' || str[i] == '1' || str[i] == '3' || str[i] == '5' || str[i] == '7' || str[i] == '8') {
+         count++;
+      }
 
-         //Находим последний элемент стека. Это нужно для того, чтобы проверить соответствует ли он паре. Если сообветствует, то мы элемент из стека удаляем т.к. найден ему соответствующий. Если нет, то последовательность неправильная и вернём false. 
-         let lastEl = stack[stack.length - 1];
+      if (str[i] == ')' || str[i] == '}' || str[i] == '|' || str[i] == '2' || str[i] == '4' || str[i] == '6' || str[i] == '7' || str[i] == '8') {
+         count--;
+      }
 
-         if (BRACKETS_PAIR[currentBreacket] === lastEl) {
-            stack.pop();
-         } else {
-            return false
-         }
+      if (count < 0) {
+         break;
       }
    }
-   //Проверяем наш стек, если он пуст, то все правильно и вернётся true, если нет, то false
-   return stack.length === 0;
+
+   if (count != 0) {
+      return false;
+   } else {
+      return true;
+   }
+
 }
